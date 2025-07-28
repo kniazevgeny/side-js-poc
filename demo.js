74af0da7f33b98d35ac6a649ff3453f536fc33fe
Eugène Kniazev <eugene.kniazev@viziosense.com>
2025-07-28 10:29:25 +0200

    upd: messages

--- demo.js
+++ demo.js
@@ -12,8 +12,17 @@ function setup() {
     document.querySelector('h1 span').textContent = 'script loaded'
     // save data only if there was a redirect from melora
     if (window.location.search.includes('utm_source=melora.ru')) {
+        document.querySelector('h1 span').textContent += ' > melora_utm detected in url'
         document.cookie = `melora_utm=${encodeURIComponent(window.location.search)}; max-age=${3600 * 24 * 30}; path=/`
         document.querySelector('h1 span').textContent += ' > cookie set'
+    } else {
+        document.querySelector('h1 span').textContent += ' > no utms detected in url'
+        const meloraUtm = document.cookie.split('; ').find(row => row.startsWith('melora_utm='))?.split('=')[1]
+        if (meloraUtm) {
+            document.querySelector('h1 span').textContent += ` > melora_utm detected in cookie: ${decodeURIComponent(meloraUtm)}`
+        } else {
+            document.querySelector('h1 span').textContent += ` > melora_utm not found in cookie`
+        }
     }
 }
 
